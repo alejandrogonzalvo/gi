22d5a5663c78015dd2162246d9daa444559ff9c6
Alejandro <alejandro@pop-os.localdomain>
2020-12-04 20:12:24 +0100

    change movement system

--- pawn.gd
+++ pawn.gd
@@ -3,10 +3,11 @@ extends Sprite
 
 var entered
 var clicked
+var movement_tiles
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 func _process(delta):
@@ -14,16 +15,24 @@ func _process(delta):
 		if Input.is_action_just_pressed("click"):
 			if clicked:
 				clicked = false
+				print('unclicked')
 			else:
 				clicked = true
-	
+				movement_tiles = $Hitbox.get_overlapping_areas()
+				for tile in movement_tiles:
+					tile.emit_signal("player_moving")
+
 	if clicked and not entered:
-		if Input.is_action_just_pressed("click"):
+		if Input.is_action_just_released("click"):
 			self.position = get_global_mouse_position()
+			clicked = false
+			print('unclicked')
 
-func _on_Area2D_mouse_entered():
+func _on_Hitbox_mouse_entered():
 	entered = true
+	print('entered')
 
 
-func _on_Area2D_mouse_exited():
+func _on_Hitbox_mouse_exited():
 	entered = false
+	print('exited')
